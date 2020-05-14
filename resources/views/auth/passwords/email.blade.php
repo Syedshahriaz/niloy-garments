85ccf3ea50fad923649e26f64feb0a084b3d18d5
Syedshahriaz <shahriaz2012@gmail.com>
2020-05-15 04:37:53 +0600

    update forget password page design

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+ {{-- @extends('layouts.app')
 
 @section('content')
 <div class="container">
@@ -34,7 +34,7 @@
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
+                                    {{ __('Send Password Reset Link asd') }}
                                 </button>
                             </div>
                         </div>
@@ -44,4 +44,134 @@
         </div>
     </div>
 </div>
-@endsection
+@endsection  --}}
+
+
+<!DOCTYPE html>
+<!--[if IE 8]> <html lang="en" class="ie8 no-js"> <![endif]-->
+<!--[if IE 9]> <html lang="en" class="ie9 no-js"> <![endif]-->
+<!--[if !IE]><!-->
+<html lang="en">
+<!--<![endif]-->
+<!-- BEGIN HEAD -->
+
+<head>
+    <meta charset="utf-8" />
+    <title>Forgot Password</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta content="width=device-width, initial-scale=1" name="viewport" />
+    <meta content="" name="description" />
+    <meta content="" name="author" />
+    <!-- BEGIN GLOBAL MANDATORY STYLES -->
+    <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&subset=all" rel="stylesheet" type="text/css" />
+    <link href="{{asset('assets/global/plugins/simple-line-icons/simple-line-icons.min.css')}}" rel="stylesheet" type="text/css" />
+    <link href="{{asset('assets/global/plugins/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet" type="text/css" />
+    <link href="{{asset('assets/global/plugins/bootstrap-switch/css/bootstrap-switch.min.css')}}" rel="stylesheet" type="text/css" />
+    <!-- END GLOBAL MANDATORY STYLES -->
+    <!-- BEGIN PAGE LEVEL PLUGINS -->
+    <link href="{{asset('assets/global/plugins/morris/morris.css')}}" rel="stylesheet" type="text/css" />
+    <!-- END PAGE LEVEL PLUGINS -->
+    <!-- BEGIN THEME GLOBAL STYLES -->
+    <link href="{{asset('assets/global/css/components-rounded.min.css')}}" rel="stylesheet" id="style_components" type="text/css" />
+    <link href="{{asset('assets/global/css/plugins.min.css')}}" rel="stylesheet" type="text/css" />
+    <!-- END THEME GLOBAL STYLES -->
+
+    <!-- BEGIN GLOBAL PAGE STYLES -->
+    <link href="{{asset('assets/pages/css/login.css')}}" rel="stylesheet" type="text/css" />
+    <link href="{{asset('assets/global/plugins/intl-tel-input-master/css/intlTelInput.min.css')}}" rel="stylesheet" type="text/css" />
+    <!-- END GLOBAL PAGE STYLES-->
+
+    <!-- BEGIN THEME LAYOUT STYLES -->
+    <link href="{{asset('assets/layouts/layout/css/layout.min.css')}}" rel="stylesheet" type="text/css" />
+    <link href="{{asset('assets/layouts/layout/css/themes/blue.min.css')}}" rel="stylesheet" type="text/css" id="style_color" />
+    <link href="{{asset('assets/layouts/layout/css/custom.css')}}" rel="stylesheet" type="text/css" />
+    <!-- END THEME LAYOUT STYLES -->
+</head>
+
+<body class=" login">
+
+<!-- BEGIN LOGIN -->
+<div class="content">
+
+    @if (session('status'))
+        <div class="alert alert-success" role="alert">
+            {{ session('status') }}
+        </div>
+    @endif
+
+    <form method="POST" class="login-form"  action="{{ route('password.email') }}">
+        @csrf
+        <!-- BEGIN LOGO -->
+        <div class="logo">
+            <a href="index.html">
+                <img src="../../assets/global/img/logo-invert.png" alt="" /> </a>
+        </div>
+        <!-- END LOGO -->
+
+        <h3 class="form-title font-theme">Recover Password</h3>
+
+        <div class="form-group">
+            <!--ie8, ie9 does not support html5 placeholder, so we just show field title for that-->
+            <label class="control-label visible-ie8 visible-ie9">Email Address</label>
+            <input class="form-control form-control-solid placeholder-no-fix @error('email') is-invalid @enderror" type="email" autocomplete="off" placeholder="Email Address" name="email" id="email" value="{{ old('email') }}" required autofocus/>                </div>
+
+
+        <div class="form-group row mb-0">
+            <div class="col-md-12">
+                <button type="submit" class="btn uppercase theme-btn pull-right">Submit</button>
+            </div>
+        </div>
+    </form>
+</div>
+
+<!--[if lt IE 9]>
+<script src="{{asset('assets/global/plugins/respond.min.js')}}"></script>
+<script src="{{asset('assets/global/plugins/excanvas.min.js')}}"></script>
+<![endif]-->
+<!-- BEGIN CORE PLUGINS -->
+<script src="{{asset('assets/global/plugins/jquery.min.js')}}" type="text/javascript"></script>
+<script src="{{asset('assets/global/plugins/bootstrap/js/bootstrap.min.js')}}" type="text/javascript"></script>
+<script src="{{asset('assets/global/plugins/bootstrap-hover-dropdown/bootstrap-hover-dropdown.min.js')}}" type="text/javascript"></script>
+<script src="{{asset('assets/global/plugins/jquery-slimscroll/jquery.slimscroll.min.js')}}" type="text/javascript"></script>
+<script src="{{asset('assets/global/plugins/jquery.blockui.min.js')}}" type="text/javascript"></script>
+<script src="{{asset('assets/global/plugins/bootstrap-switch/js/bootstrap-switch.min.js')}}" type="text/javascript"></script>
+<!-- END CORE PLUGINS -->
+
+<!-- BEGIN PAGE LEVEL PLUGINS -->
+<script src="{{asset('assets/global/plugins/moment.min.js')}}" type="text/javascript"></script>
+<script src="{{asset('assets/global/plugins/morris/morris.min.js')}}" type="text/javascript"></script>
+<script src="{{asset('assets/global/plugins/counterup/jquery.waypoints.min.js')}}" type="text/javascript"></script>
+<script src="{{asset('assets/global/plugins/counterup/jquery.counterup.min.js')}}" type="text/javascript"></script>
+
+<!--For chart START-->
+<script src="{{asset('assets/global/plugins/flot/jquery.flot.min.js')}}" type="text/javascript"></script>
+<script src="{{asset('assets/global/plugins/flot/jquery.flot.categories.min.js')}}" type="text/javascript"></script>
+<!--For chart END-->
+<!-- END PAGE LEVEL PLUGINS -->
+
+<!-- BEGIN THEME GLOBAL SCRIPTS -->
+<script src="{{asset('assets/global/scripts/app.min.js')}}" type="text/javascript"></script>
+<!-- input with country flag & code-->
+<script src="{{asset('assets/global/plugins/intl-tel-input-master/js/intlTelInput-jquery.min.js')}}" type="text/javascript"></script>
+<!-- For Documentation https://github.com/jackocnr/intl-tel-input#demo-and-examples-->
+<!-- END THEME GLOBAL SCRIPTS -->
+
+<!-- BEGIN THEME LAYOUT SCRIPTS -->
+<script src="{{asset('assets/layouts/layout/scripts/layout.min.js')}}" type="text/javascript"></script>
+<!-- END THEME LAYOUT SCRIPTS -->
+
+<!-- BEGIN PAGE LEVEL SCRIPTS -->
+<script src="{{asset('assets/pages/scripts/dashboard.js')}}" type="text/javascript"></script>
+<!-- END PAGE LEVEL SCRIPTS -->
+
+<!-- BEGIN GLOABL CUSTOM SCRIPTS -->
+<script src="{{asset('assets/global/scripts/custom.js')}}" type="text/javascript"></script>
+<!-- END GLOABL CUSTOM SCRIPTS -->
+
+<script type="text/javascript">
+    
+</script>
+
+</body>
+
+</html>
