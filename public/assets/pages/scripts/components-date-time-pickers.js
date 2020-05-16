88d0cbce807f7dd906040a2dd7acad3bd1a9fb03
Syedshahriaz <shahriaz2012@gmail.com>
2020-05-16 17:37:02 +0600

    update FE changes

--- public/assets/pages/scripts/components-date-time-pickers.js
+++ public/assets/pages/scripts/components-date-time-pickers.js
@@ -9,6 +9,9 @@ var ComponentsDateTimePickers = function () {
                 autoclose: true,
                 format: 'DD dd, MM, yyyy',
             });
+            $('.date-picker').on('changeDate', function(e){
+                $(this).next('input[type=hidden].date-picker-hidden').val( moment(e.date).format('DD-MM-YYYY') );
+            });
             //$('body').removeClass("modal-open"); // fix bug when inline picker is used in modal
         }
 
--- resources/views/user/project/select_shipment.blade.php
+++ resources/views/user/project/select_shipment.blade.php
@@ -70,14 +70,10 @@
 
                                 <div class="row">
                                     <div class="col-md-10 col-md-offset-1">
-                                        <div class="alert alert-danger text-center" role="alert">
-                                            Select <b>Shipment Date and Gender</b>
-                                        </div>
-                                    </div>
-                                    <div class="col-md-10 col-md-offset-1">
                                         <div class="form-group">
                                             <label for=""><b>Shipping Date</b></label>
                                             <input class="form-control date-picker" size="16" type="text" name="shipment_date" id="shipment_date" value="" placeholder="Select Shipping Date"/>
+                                            <input class="date-picker-hidden" type="hidden"/>   
                                         </div>
                                     </div>
                                     <div class="col-md-10 col-md-offset-1">
