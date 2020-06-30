83cd777593f78669ed7cbc489aea7627c08b222d
Muhin <muhin@blubirdinteractive.com>
2020-06-30 17:44:50 +0600

    User task title update issue fixing

--- app/Models/UserProject.php
+++ app/Models/UserProject.php
@@ -15,8 +15,9 @@ class UserProject extends Model
     public function tasks()
     {
         $instance = $this->hasMany('App\Models\UserProjectTask','user_project_id','user_project_id');
-        $instance = $instance->select('user_project_tasks.*', 'tasks.title', 'tasks.rule', 'tasks.status as task_status', 'tasks.project_id','tasks.has_freeze_rule');
+        $instance = $instance->select('user_project_tasks.*', 'task_title.name as title', 'tasks.rule', 'tasks.status as task_status', 'tasks.project_id','tasks.has_freeze_rule');
         $instance = $instance->join('tasks','tasks.id','user_project_tasks.task_id');
+        $instance = $instance->join('task_title', 'task_title.id', '=', 'tasks.title_id');
         $instance = $instance->where('tasks.status','active');
         return $instance;
     }
@@ -24,8 +25,9 @@ class UserProject extends Model
     public function running_task()
     {
         $instance = $this->hasOne('App\Models\UserProjectTask','user_project_id','user_project_id');
-        $instance = $instance->select('user_project_tasks.*','tasks.title','tasks.has_freeze_rule');
+        $instance = $instance->select('user_project_tasks.*', 'task_title.name as title','tasks.has_freeze_rule');
         $instance = $instance->join('tasks','tasks.id','user_project_tasks.task_id');
+        $instance = $instance->join('task_title', 'task_title.id', '=', 'tasks.title_id');
         $instance = $instance->where('tasks.status','active');
         $instance = $instance->where('user_project_tasks.status','processing');
         //$instance = $instance->orderBy('user_project_tasks.id','ASC');
@@ -36,8 +38,9 @@ class UserProject extends Model
     public function last_task()
     {
         $instance = $this->hasOne('App\Models\UserProjectTask','user_project_id','user_project_id');
-        $instance = $instance->select('user_project_tasks.*','tasks.title','tasks.has_freeze_rule');
+        $instance = $instance->select('user_project_tasks.*', 'task_title.name as title','tasks.has_freeze_rule');
         $instance = $instance->join('tasks','tasks.id','user_project_tasks.task_id');
+        $instance = $instance->join('task_title', 'task_title.id', '=', 'tasks.title_id');
         $instance = $instance->where('tasks.status','active');
         $instance = $instance->orderBy('user_project_tasks.id','DESC');
         //$instance = $instance->limit(1);
