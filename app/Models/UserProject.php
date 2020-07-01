af8d3caa6bb2a39362fe13b70651a4e1d2978ce8
Muhin <muhin@blubirdinteractive.com>
2020-07-01 14:26:30 +0600

    Making admin user status dynamic

--- app/Models/UserProject.php
+++ app/Models/UserProject.php
@@ -29,6 +29,7 @@ class UserProject extends Model
         $instance = $instance->join('tasks','tasks.id','user_project_tasks.task_id');
         $instance = $instance->join('task_title', 'task_title.id', '=', 'tasks.title_id');
         $instance = $instance->where('tasks.status','active');
+        $instance = $instance->where('tasks.has_freeze_rule',0);
         $instance = $instance->where('user_project_tasks.status','processing');
         //$instance = $instance->orderBy('user_project_tasks.id','ASC');
         //$instance = $instance->limit(1);
@@ -42,7 +43,36 @@ class UserProject extends Model
         $instance = $instance->join('tasks','tasks.id','user_project_tasks.task_id');
         $instance = $instance->join('task_title', 'task_title.id', '=', 'tasks.title_id');
         $instance = $instance->where('tasks.status','active');
+        $instance = $instance->where('tasks.has_freeze_rule',0);
         $instance = $instance->orderBy('user_project_tasks.id','DESC');
+        //$instance = $instance->limit(1);
+        return $instance;
+    }
+
+    public function passed_task()
+    {
+        $today = date('Y-m-d');
+        $instance = $this->hasOne('App\Models\UserProjectTask','user_project_id','id');
+        $instance = $instance->join('tasks','tasks.id','user_project_tasks.task_id');
+        $instance = $instance->where('original_delivery_date','<',$today);
+        $instance = $instance->where('tasks.status','active');
+        $instance = $instance->where('tasks.has_freeze_rule',0);
+        $instance = $instance->where('user_project_tasks.status','processing');
+        //$instance = $instance->limit(1);
+        return $instance;
+    }
+
+    public function recent_due_task()
+    {
+        $today = date('Y-m-d');
+        $lastDay = date('Y-m-d', strtotime('+6 days'));
+
+        $instance = $this->hasOne('App\Models\UserProjectTask','user_project_id','id');
+        $instance = $instance->join('tasks','tasks.id','user_project_tasks.task_id');
+        $instance = $instance->whereBetween('original_delivery_date',[$today,$lastDay]);
+        $instance = $instance->where('tasks.status','active');
+        $instance = $instance->where('tasks.has_freeze_rule',0);
+        $instance = $instance->where('user_project_tasks.status','processing');
         //$instance = $instance->limit(1);
         return $instance;
     }
