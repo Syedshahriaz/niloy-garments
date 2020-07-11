e340e3e93f31072fa6bd5210733a7a2147e69ca1
Syedshahriaz <shahriaz2012@gmail.com>
2020-07-12 05:48:32 +0600

    update message page

--- resources/views/partials/scripts.blade.php
+++ resources/views/partials/scripts.blade.php
@@ -49,6 +49,7 @@
 <!-- END PAGE LEVEL PLUGINS -->
 
 <script src="{{ asset('assets/holdon/holdon.min.js')}}"></script>
+<script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.js"></script>
 
 <!-- BEGIN GLOABL CUSTOM SCRIPTS -->
 <script src="{{asset('assets/global/scripts/custom.js')}}" type="text/javascript"></script>
