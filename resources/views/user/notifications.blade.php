9702c21b6ec62985ebcdbf0715ab2e50ca610e4e
Muhin <muhin@blubirdinteractive.com>
2020-08-11 09:38:43 +0600

    Adding notification serial number

--- resources/views/user/notifications.blade.php
+++ resources/views/user/notifications.blade.php
@@ -61,7 +61,7 @@
                                     <td>{{$notification->message}}</td>
                                     <td>
                                         @if($notification->created_at !='')
-                                            {{date('l d, M, Y', strtotime($user->created_at))}}
+                                            {{date('l d, M, Y', strtotime($notification->created_at))}}
                                         @endif
                                     </td>
                                 </tr>
