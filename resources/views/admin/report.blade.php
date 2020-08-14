f48d1cd96edb7c33dbbd30ba4fa2935a85202788
Syedshahriaz <shahriaz2012@gmail.com>
2020-08-14 07:27:37 +0600

    update report

--- resources/views/admin/report.blade.php
+++ resources/views/admin/report.blade.php
@@ -28,11 +28,47 @@
             <div class="row mt-3">
                 <div class="col-md-12 col-sm-12">
                     <!-- BEGIN PORTLET-->
-                    <div class="portlet light ">
-                        <div class="portlet-body" style="padding-top: 0;">
-                            ghjghj
+                    <!-- BEGIN PORTLET-->
+                    <div class="portlet light bordered">
+                        <div class="portlet-title">
+                            <div class="caption">
+                                <i class="icon-share font-red-sunglo hide"></i>
+                                <span class="caption-subject font-dark bold uppercase">Report Name</span>
+                                <span class="caption-helper"></span>
+                            </div>
+                            <div class="actions hidden" id="action_buttons">
+                               <button type="button" class="btn btn-transparent theme-btn btn-circle btn-sm" title="Send Email" id="send_email_all">Send Email</button>
+                               {{--<button type="button" class="btn btn-transparent theme-btn btn-circle btn-sm" title="Remove Users" id="delete_user_all">Delete Users</button>--}}
+                            </div>
+                        </div>
+                        <div class="portlet-body p-relative">
+                            <table id="report_table" class="table table-striped table-bordered table-hover data-table focus-table">
+                                <thead>
+                                    <tr>
+                                        <th class="text-center">
+                                            Total Visitor
+                                        </th>
+                                        <th class="text-center">Total Male Visitor</th>
+                                        <th class="text-center">Total Female Visitor</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td class="text-center">
+                                            2000
+                                        </td>
+                                        <td class="text-center">
+                                            1200
+                                        </td>
+                                        <td class="text-center">
+                                            800
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
                         </div>
                     </div>
+                    <!-- END PORTLET-->
                 </div>
             </div>
 
