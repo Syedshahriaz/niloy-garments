5c829aee38cbc52ef155150ba86fbff214fbdd32
Muhin <muhin@blubirdinteractive.com>
2020-09-03 16:37:04 +0600

    Sending birthay wish sms to user

--- app/Http/Controllers/CronController.php
+++ app/Http/Controllers/CronController.php
@@ -50,4 +50,35 @@ class CronController extends Controller
         }
     }
 
+    public function sendBirthdayWish(Request $request){
+        try{
+            $users = User::select('users.phone')
+                ->join('user_shipments','user_shipments.user_id','=','users.id')
+                ->whereRaw('MONTH(shipment_date) = MONTH(NOW())')
+                ->whereRaw('DAY(shipment_date) = DAY(NOW())')
+                ->pluck('users.phone')
+                ->toArray();
+
+            $mobile_nos = implode(',',$users);
+
+            foreach($users as $user){
+                $message_body = 'May your birthday be as beautiful, wonderful as you are. ';
+                $message_body.='I pray to God that each candle on the cake convert into wish and May all of them come true. ';
+                $message_body.='Happy Birthday!';
+
+                $campaign_title = 'Happy Birthday';
+
+                $response = SMS::sendCampaignSms($mobile_nos,$message_body,$campaign_title);
+
+                return $response;
+            }
+        }
+        catch (\Exception $e) {
+            //SendMails::sendErrorMail($e->getMessage(), null, 'CronController', 'sendSubscriptionRenewWarningEmail', $e->getLine(),
+                //$e->getFile(), '', '', '', '');
+            // message, view file, controller, method name, Line number, file,  object, type, argument, email.
+            return [ 'status' => 401, 'reason' => 'Something went wrong. Try again later'];
+        }
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('cron/send_task_warning_email', 'CronController@sendTaskWarningEmail')->name('cron.send_task_warning_email');
+Route::get('cron/send_birthday_wish', 'CronController@sendBirthdayWish')->name('cron.send_birthday_wish');
 
 /*
  * Admin auth route
